5204dd695d214362f44314ea8ba8c97af2949f46
Rathakrishnan <born4winkrishna@gmail.com>
2022-04-26 15:58:47 +0800

    Initial Commit

---
+++ SwiftInstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftInstagram
+//
+//  Created by Rathakrishnan Ramasamy on 26/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
